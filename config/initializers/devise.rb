1c3cc98d039b0b7c6aebad20562fd4b8874d6539
浜野秋仁 <s.sakae3354@gmail.com>
2018-07-29 13:28:25 +0900

    Delete unnecessary line

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -121,7 +121,6 @@ Devise.setup do |config|
   # config.send_password_change_notification = false
   # encryptor), the cost increases exponentially with the number of stretches (e.g.
   # a value of 20 is already extremely slow: approx. 60 seconds for 1 calculation).
-  config.stretches = Rails.env.test? ? 1 : 10
 
   # Setup a pepper to generate the encrypted password.
   # config.pepper = 'e631ea2ef98eb22a01a2b28c3833d8fbdd4391cfbd2ff8a2a6fe471a59ccad6216df86e84c99e5708436087cfa5bb49ce84021af374d36a5c852d64878c607c3'
