d95ad84f0e26e3860caab540d4c615edf36821f7
ylolu21 <ylolu21@gmail.com>
2018-08-04 16:32:34 +0900

    Fixing preview.js

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,31 +1,38 @@
 $(function(){
-  $('form').on('change', 'input[type="file"]', function(e) {
-    var file = e.target.files[0],
-        reader = new FileReader(),
-        $preview = $(".cover-image-upload");
-        t = this;
+  $(".col-md-12").on('change', function() {
+    var file = $(this).prop('files')[0],
+        filereader = new FileReader(),
+        find_img = $(this).find("img"),
+        view_box = $(this);
 
-    // 画像ファイル以外の場合は何もしない
-    if(file.type.indexOf("image") < 0){
-      return false;
+    if(find_img.length){
+       find_img.nextAll().remove();
+       find_img.remove();
     }
 
     // ファイル読み込みが完了した際のイベント登録
-    reader.onload = (function(file) {
+    filereader.onload = (function(file) {
       return function(e) {
         //既存のプレビューを削除
-        $preview.empty();
+        // $find_img.remove();
         // .prevewの領域の中にロードした画像を表示するimageタグを追加
-        $preview.append($('<img>').attr({
+        $(".cover-image-upload").prepend($('<img>').attr({
                   src: e.target.result,
-                  // width: "100%",
+                  width: "100%",
                   height: "500px",
-                  class: "cover-image-upload",
+                  // class: "cover-image-upload",
+                  title: file.name
+              }));
+        $(".image-upload").prepend($('<img>').attr({
+                  src: e.target.result,
+                  width: "100%",
+                  height: "100%",
+                  // class: "cover-image-upload",
                   title: file.name
               }));
       };
     })(file);
 
-    reader.readAsDataURL(file);
+    filereader.readAsDataURL(file);
   });
 });
