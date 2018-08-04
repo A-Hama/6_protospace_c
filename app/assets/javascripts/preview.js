6930aa5f71df4856ad057b6d2967176b67a61836
ylolu21 <ylolu21@gmail.com>
2018-08-04 13:53:10 +0900

    Add preview.js

---
+++ app/assets/javascripts/preview.js
@@ -0,0 +1,31 @@
+$(function(){
+  $('form').on('change', 'input[type="file"]', function(e) {
+    var file = e.target.files[0],
+        reader = new FileReader(),
+        $preview = $(".cover-image-upload");
+        t = this;
+
+    // 画像ファイル以外の場合は何もしない
+    if(file.type.indexOf("image") < 0){
+      return false;
+    }
+
+    // ファイル読み込みが完了した際のイベント登録
+    reader.onload = (function(file) {
+      return function(e) {
+        //既存のプレビューを削除
+        $preview.empty();
+        // .prevewの領域の中にロードした画像を表示するimageタグを追加
+        $preview.append($('<img>').attr({
+                  src: e.target.result,
+                  // width: "100%",
+                  height: "500px",
+                  class: "cover-image-upload",
+                  title: file.name
+              }));
+      };
+    })(file);
+
+    reader.readAsDataURL(file);
+  });
+});
