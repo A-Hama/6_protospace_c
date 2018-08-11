81e70c1a89eee4c56b7f3d51545524e4d14e6fd4
ylolu21 <ylolu21@gmail.com>
2018-08-11 13:04:04 +0900

    introduction of javascript

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,33 +1,26 @@
 $(function(){
-  $(".col-md-12").on('change', function() {
+  $(".image_file").on('change', function() {
+    console.log(this);
     var file = $(this).prop('files')[0],
-        filereader = new FileReader(),
-        find_img = $(this).find("img"),
-        view_box = $(this);
+        find_img = $(this).parent().find("img"),
+        filereader = new FileReader();
+        var view_box = $(this).parent();
 
     if(find_img.length){
-       find_img.nextAll().remove();
        find_img.remove();
     }
 
-    // ファイル読み込みが完了した際のイベント登録
     filereader.onload = function() {
 
-        $(".cover-image-upload").prepend($('<img>').attr({
-                  src: e.target.result,
-                  width: "100%",
-                  height: "500px",
-                  // class: "cover-image-upload",
-                  title: file.name
-              }));
-        $(".image-upload").prepend($('<img>').attr({
-                  src: e.target.result,
+        $(view_box).prepend($('<img>').attr({
+                  src: filereader.result,
                   width: "100%",
                   height: "100%",
-                  // class: "cover-image-upload",
                   title: file.name
               }));
       }
     filereader.readAsDataURL(file);
   });
 });
+
+
