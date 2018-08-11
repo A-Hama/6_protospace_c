75c6f6f315d6b9923ca7bc6b83b128949156a990
浜野秋仁 <s.sakae3354@gmail.com>
2018-08-11 16:57:45 +0900

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-<<<<<<< HEAD
-  resources :prototypes, only: [:index, :new, :create, :show, :destroy] do
+
+  resources :prototypes do
     resources :likes, only: [:create, :destroy]
   end
-=======
-  resources :prototypes
->>>>>>> master
+  
   resources :users, only: [:show, :edit, :update]
 end
