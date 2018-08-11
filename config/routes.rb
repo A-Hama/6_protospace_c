d22d4b0f9da2949c2f5cc6b978c44e2950f08ac4
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-08-11 16:04:27 +0900

    set routing for popular and newest

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
+  get 'prototypes/popular'
+  get 'prototypes/newest'
   resources :prototypes
   resources :users, only: [:show, :edit, :update]
 end
