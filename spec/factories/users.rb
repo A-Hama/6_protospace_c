5e107dcf1d5a111116e01c84b114c7e8ff6bcf61
ylolu21 <ylolu21@gmail.com>
2018-08-25 16:02:52 +0900

    Add user instance

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+
+  factory :user do
+    name                  "abe"
+    email                 "kkk@gmail.com"
+    password              "00000000"
+    password_confirmation "00000000"
+    profile               "aaa"
+    position              "aaa"
+    occupation            "aaa"
+  end
+
+end
