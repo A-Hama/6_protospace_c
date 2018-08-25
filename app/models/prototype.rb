422d65602f0aa4b6f4445aa911842726a516801a
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-08-25 16:26:09 +0900

    edit models/prototype.rb
    リレーションを設定するとともに、controllerで行う処理をsave_prototypesメソッドにまとめた。

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,6 +2,8 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :captured_images, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :tag_maps, dependent: :destroy
+  has_many :tags, through: :tag_maps
 
   accepts_nested_attributes_for :captured_images, reject_if: :reject_sub_images
 
@@ -25,6 +27,21 @@ class Prototype < ActiveRecord::Base
 
   def posted_date
     created_at.strftime('%b %d %a')
+  end
+
+  def save_prototypes(tags_list)
+    current_tags = self.tags.pluck(:name) unless self.tags.nil?
+    old_tags = current_tags - tags_list
+    new_tags = tags_list - current_tags
+
+    old_tags.each do |old_name|
+      self.tags.delete Tag.find_by(name: old_name)
+    end
+
+    new_tags.each do |new_name|
+      post_tag = Tag.find_or_create_by(name: new_name)
+      self.tags << post_tag
+    end
   end
 
   scope :popular, -> {order('like_count DESC')}
