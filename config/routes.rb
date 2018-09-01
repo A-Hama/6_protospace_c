c52eb7cee5be28d97fdc072630d03dc2f2ae043f
nonkikaba <rakurakuhuton.20011@gmail.com>
2018-09-01 16:52:25 +0900

    　add routing to destroy
    ルーティングにdestroyアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   
-  resources :tags, only: [:index, :show]
+  resources :tags, only: [:index, :show, :destroy]
   resources :prototypes do
     resources :likes, only: [:create, :destroy]
     collection do 
