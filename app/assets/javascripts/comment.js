2f060dfcd8bb212d89c3ad0a7276d8b29bdbe2e5
浜野秋仁 <s.sakae3354@gmail.com>
2018-09-01 10:30:18 +0900

    Add routing to comments

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,36 +1,49 @@
 $(function(){
   function buildHTML(comment){
-    var html = `<p>
+    var html = `<div class="comment" data-comment-id="${comment.id}">
                   <strong>
                     <a href=/users/${comment.user_id}>${comment.user_name}</a>
-                    ：
                   </strong>
-                  ${comment.text}
-                </p>`
+                    <a rel="nofollow" data-method="delete" href="/prototypes/${comment.prototype_id}/comments/${comment.id}">
+                      <div class="button btn-outline-primary">
+                        <div class="glyphicon glyphicon-trash">
+                        削除
+                        </div>
+                      </div>
+                    </a>
+                    <a data-method="get" href="/prototypes/${comment.prototype_id}/comments/${comment.id}/edit">
+                      <div class="button btn-outline-primary">
+                        <div class="glyphicon glyphicon-pencil">
+                          編集
+                        </div>
+                      </div>
+                    </a>
+                    <p>${comment.content}</p>
+                </div>
+                `
     return html;
   }
+
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var href = window.location.href
-    console.log(href);
-    console.log(formData);
+    var url = $(this).attr('action');
     $.ajax({
-      url: href,
+      url: url,
       type: "POST",
       data: formData,
       dataType: 'json',
-      contentType: false
+      contentType: false,
       processData: false
     })
+
     .done(function(data){
       var html = buildHTML(data);
-      $('.comments').append(html)
+      $('.comments').append(html);
       $('.textbox').val('')
     })
     .fail(function(){
-      console.log(this);
       alert('error');
     })
-  })
+  });
 });
